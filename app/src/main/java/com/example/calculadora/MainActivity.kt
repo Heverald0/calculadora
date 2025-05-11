69a9996b4fdefaa2d8fda57315afb8b48f675053
Heveraldo Augusto <Heveraldoa8@gmail.com>
2025-05-11 12:33:37 -0300

    Inicialização de tela e leituras dos IDs

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -9,5 +9,31 @@ import androidx.core.view.WindowInsetsCompat
 import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
-    
+
+    private var mr = 0.0
+    private var temp1 = 0.0
+    private var temp2 = 0.0
+    private var operacao = 0
+    private var result = 0.0
+    private var isResult = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val display = findViewById<TextView>(R.id.display)
+
+        configurarNumeros(display)
+        configurarMemoria(display)
+        configurarFuncoesCientificas(display)
+        configurarOperacoes(display)
+        configurarResultado(display)
+    }
 }
